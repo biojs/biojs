c9027b48f0b4093b2ab79fadd963f7528f60fa16
John Gomez <johncar@gmail.com>
2012-01-18 12:40:02 +0000

    Example data

---
+++ src/doc/biojsTemplate/resources/data/uniprotFeaturesJSON.js
@@ -0,0 +1,44 @@
+{"featuresArray":[
+            {"nonOverlappingStyle":{"heightOrRadius":10,"y":90},"type":"rect","featureEnd":336,"fillOpacity":0.5,
+            "evidenceText":"InterPro","stroke":"#033158","height":10,"path":"","typeLabel":"Domain",
+            "featureLabel":"HAD-like domain","featureStart":64,"strokeWidth":1,"text":"",
+            "centeredStyle":{"heightOrRadius":8,"y":80},"fill":"#033158","width":486,
+            "typeCategory":"inferred from InterPro motif similarity (ECO:0000029)",
+            "typeCode":"SO:0000417","cy":90,"cx":134,"evidenceCode":"HAD-like domain","r":10,
+            "featureId":"IPR023214_64_336","rowsStyle":{"heightOrRadius":10,"y":125},
+            "featureTypeLabel":"polypeptide_domain","y":90,"x":134},
+            {"nonOverlappingStyle":{"heightOrRadius":5,"y":45},"type":"circle","featureEnd":90,"fillOpacity":0.5,"evidenceText":"UniProt","stroke":"#FF6666","height":5,"path":"","typeLabel":"Metal binding","featureLabel":"Magnesium; via carbonyl oxygen","featureStart":90,"strokeWidth":1,"text":"","centeredStyle":{"heightOrRadius":5,"y":135},"fill":"#FF6666","width":0,"typeCategory":"Site","typeCode":"SO:0001092","cy":45,"cx":181,"evidenceCode":"","r":5,"featureId":"UNIPROTKB_Q9H0P0_METAL_90_90","rowsStyle":{"heightOrRadius":5,"y":40},"featureTypeLabel":"metal_contact","y":45,"x":181},
+            {"nonOverlappingStyle":{"heightOrRadius":5,"y":55},"type":"circle","featureEnd":88,"fillOpacity":0.5,"evidenceText":"UniProt","stroke":"#FF6666","height":5,"path":"","typeLabel":"Metal binding","featureLabel":"Magnesium","featureStart":88,"strokeWidth":1,"text":"","centeredStyle":{"heightOrRadius":5,"y":125},"fill":"#FF6666","width":0,"typeCategory":"Site","typeCode":"SO:0001092","cy":55,"cx":177,"evidenceCode":"","r":5,"featureId":"UNIPROTKB_Q9H0P0_METAL_88_88","rowsStyle":{"heightOrRadius":5,"y":40},"featureTypeLabel":"metal_contact","y":55,"x":177},
+            {"nonOverlappingStyle":{"heightOrRadius":5,"y":60},"type":"diamond","featureEnd":90,"fillOpacity":0.5,"evidenceText":"UniProt","stroke":"#760000","height":5,"path":"","typeLabel":"Active site","featureLabel":"Proton donor","featureStart":90,"strokeWidth":1,"text":"","centeredStyle":{"heightOrRadius":5,"y":110},"fill":"#760000","width":0,"typeCategory":"Site","typeCode":"SO:0001104","cy":60,"cx":181,"evidenceCode":"","r":5,"featureId":"UNIPROTKB_Q9H0P0_ACT_SITE_90_90","rowsStyle":{"heightOrRadius":5,"y":45},"featureTypeLabel":"catalytic_residue","y":60,"x":181},
+            {"nonOverlappingStyle":{"heightOrRadius":5,"y":65},"type":"circle","featureEnd":277,"fillOpacity":0.5,"evidenceText":"UniProt","stroke":"#FF6666","height":5,"path":"","typeLabel":"Metal binding","featureLabel":"Magnesium","featureStart":277,"strokeWidth":1,"text":"","centeredStyle":{"heightOrRadius":5,"y":115},"fill":"#FF6666","width":0,"typeCategory":"Site","typeCode":"SO:0001092","cy":65,"cx":515,"evidenceCode":"","r":5,"featureId":"UNIPROTKB_Q9H0P0_METAL_277_277","rowsStyle":{"heightOrRadius":5,"y":40},"featureTypeLabel":"metal_contact","y":65,"x":515},
+            {"nonOverlappingStyle":{"heightOrRadius":5,"y":70},"type":"diamond","featureEnd":88,"fillOpacity":0.5,"evidenceText":"UniProt","stroke":"#760000","height":5,"path":"","typeLabel":"Active site","featureLabel":"Nucleophile","featureStart":88,"strokeWidth":1,"text":"","centeredStyle":{"heightOrRadius":5,"y":100},"fill":"#760000","width":0,"typeCategory":"Site","typeCode":"SO:0001104","cy":70,"cx":177,"evidenceCode":"","r":5,"featureId":"UNIPROTKB_Q9H0P0_ACT_SITE_88_88","rowsStyle":{"heightOrRadius":5,"y":45},"featureTypeLabel":"catalytic_residue","y":70,"x":177}
+            ],
+        "segment":"Q9H0P0",
+        "legend":{
+            "segment":{"yPosCentered":190,"text":"Q9H0P0","yPos":140,"xPos":7,"yPosNonOverlapping":140,"yPosRows":270},
+            "key":[
+                {"label":
+                    {"total":"2","yPosCentered":210,"text":"Active site","yPos":160,"xPos":34,
+                    "yPosNonOverlapping":160,"yPosRows":290},
+                  "shape":
+                    {"centeredStyle":{"heightOrRadius":5,"y":150},"text":"",
+                    "nonOverlappingStyle":{"heightOrRadius":5,"y":150},"width":0,"fill":"#760000","cy":150,
+                    "cx":14,"type":"diamond","fillOpacity":0.5,"stroke":"#760000","height":5,"r":5,"path":"",
+                    "rowsStyle":{"heightOrRadius":5,"y":285},"typeLabel":"Active site","y":150,"strokeWidth":1,"x":14}
+                 },
+                {"label":{"total":"1","yPosCentered":210,"text":"Domain","yPos":160,"xPos":177,"yPosNonOverlapping":160,"yPosRows":290},"shape":{"centeredStyle":{"heightOrRadius":5,"y":155},"text":"","nonOverlappingStyle":{"heightOrRadius":5,"y":155},"width":22,"fill":"#033158","cy":155,"cx":150,"type":"rect","fillOpacity":0.5,"stroke":"#033158","height":5,"r":10,"path":"","rowsStyle":{"heightOrRadius":5,"y":288},"typeLabel":"Domain","y":155,"strokeWidth":1,"x":150}},
+                {"label":{"total":"3","yPosCentered":210,"text":"Metal binding","yPos":160,"xPos":327,"yPosNonOverlapping":160,"yPosRows":290},"shape":{"centeredStyle":{"heightOrRadius":5,"y":155},"text":"","nonOverlappingStyle":{"heightOrRadius":5,"y":155},"width":0,"fill":"#FF6666","cy":155,"cx":307,"type":"circle","fillOpacity":0.5,"stroke":"#FF6666","height":5,"r":5,"path":"","rowsStyle":{"heightOrRadius":5,"y":290},"typeLabel":"Metal binding","y":155,"strokeWidth":1,"x":307}}]
+        },
+        "configuration":{
+            "requestedStop":336,"requestedStart":1,
+            "horizontalGridNumLines":6,"sequenceLineYCentered":95,"gridLineHeight":10,"rightMargin":20,
+            "belowRuler":30,"sequenceLength":336,"horizontalGridNumLinesNonOverlapping":6,
+            "horizontalGridNumLinesCentered":6,"sizeYNonOverlapping":110,"verticalGridLineLengthRows":240,
+            "unitSize":1.7857142857142858,"style":"nonOverlapping","sequenceLineYRows":135,"sequenceLineY":90,
+            "verticalGrid":false,"rulerY":20,"horizontalGrid":false,"pixelsDivision":50,"sizeY":110,"sizeX":640,
+            "sizeYRows":240,"aboveRuler":10,"rulerLength":600,"verticalGridLineLengthNonOverlapping":100,
+            "sizeYKey":100,"sizeYCentered":160,"sequenceLineYNonOverlapping":90,"verticalGridLineLength":100,
+            "leftMargin":20,"horizontalGridNumLinesRows":10,"nonOverlapping":true,
+            "dasReference":"http://www.ebi.ac.uk/das-srv/uniprot/das/uniprot",
+            "dasSources":"http://www.ebi.ac.uk/das-srv/uniprot/das/uniprot,http://www.ebi.ac.uk/das-srv/interpro/das/InterPro-matches-overview",
+            "verticalGridLineLengthCentered":170}}
