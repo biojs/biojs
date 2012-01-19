6f1d241d286b2a3d4fcf2d695c8036fc3792bfcf
Leyla <leylajael@gmail.com>
2012-01-19 11:14:31 +0000

    Testing painter with proxy: using web service url at jweb-5b

--- src/main/javascript/Biojs.UniProtFeaturePainter.js
+++ src/main/javascript/Biojs.UniProtFeaturePainter.js
@@ -87,7 +87,7 @@ var Biojs_UniProtFeaturePainter_myself = undefined;
  * jQuery.ajax({
  * 		//url: 'data/uniprotFeaturesJSON.js',
  * 	    url: '../biojs/dependencies/proxy/proxy.php',
- * 	    data: 'url=http://localhost:8080/image?segment=a4_human%3Fstyle=nonOverlapping',
+ * 	    data: 'url=http://jweb-5b.ebi.ac.uk:21280/das_uniprot_cartoon/image?segment=a4_human&style=nonOverlapping',
  *      dataType: 'json',
  *      success: function(response) {           	   
  *      	Biojs.console.log(response);
