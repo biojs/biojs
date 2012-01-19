c67e8ee9d4fcf275a36949d8b8b1f33495f96b3a
John Gomez <johncar@gmail.com>
2012-01-19 11:06:02 +0000

    Added support for POST req

--- src/main/resources/dependencies/proxy/proxy.php
+++ src/main/resources/dependencies/proxy/proxy.php
@@ -1,7 +1,10 @@
 <?php
 
 /* SPLIT PROXY FROM URL TO QUERY. QUERY: ALL INSIDE THE 'S' PARAMETER */
-        $url = str_replace(" ", "%20", $_GET['url']);
+        //$url = str_replace(" ", "%20", $_GET['url']);
+        $url = ($_POST['url']) ? $_POST['url'] : $_GET['url'];
+		$headers = ($_POST['headers']) ? $_POST['headers'] : $_GET['headers'];
+		$mimeType =($_POST['mimeType']) ? $_POST['mimeType'] : $_GET['mimeType'];
 
 /* SET INTERNAL PROXY */
         $ebiDomainFlag = strpos($_SERVER['SERVER_NAME'], "ebi.ac.uk");
@@ -11,37 +14,64 @@
         } else {
         	$proxy = "";
         }
- 		
+		
         //$proxy = "http://wwwcache.ebi.ac.uk:3128/";
         //$proxy = "http://wwwcache.sanger.ac.uk:3128/";
 
+		$ch = curl_init();
+		
+// SET DATA 
+		$data = ($_POST['url']) ? $_POST : $_GET;
+		unset($data['url']);
+
+		$query = '';
+		while ($element = current($data)) {
+		   $query .= key($data).'='.$element.'&';
+		   next($data);
+		}
+
+		if ($_POST['url']) {
+			 curl_setopt ($session, CURLOPT_POST, true);
+			 curl_setopt ($session, CURLOPT_POSTFIELDS, $query);
+		} else {
+			$url .= ( strpos($url, '?') ) ? '&'.$query : '?'.$query ;
+		}
+
 
 /* CURL CONFIGURARTION */
-        $ch = curl_init();
+        
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 50);
         curl_setopt($ch, CURLOPT_TIMEOUT, 50);
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		
         if(strlen($proxy) != 0){
-                //curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1); 
-                curl_setopt($ch, CURLOPT_PROXY, "$proxy");
+            curl_setopt($ch, CURLOPT_PROXY, "$proxy");
         }
         /* Don't return HTTP headers. Do return the contents of the call */     
         curl_setopt($ch, CURLOPT_HEADER, 0);
-
-
+		$response = curl_exec($ch);
+		
 /* DISPLAY DATA FROM THE ORIGINAL QUERY */
 
-		$xmlFlag = strpos($_SERVER['HTTP_ACCEPT'], "xml"); 
+		if ($mimeType == "")
+		{
+			if ( strpos($_SERVER['SERVER_NAME'], "xml") ) {
+				$mimeType = "application/xml";
+			} else if ( strpos($_SERVER['SERVER_NAME'], "json") ) {
+				$mimeType = "application/json";
+			} else {
+				$mimeType = "text/plain";
+			}
+			
+		} 
+		
+		// The web service returns XML. Set the Content-Type appropriately
+		header("Content-Type: ".$mimeType);
 
-		//$xmlFlag = strpos($url, "stylesheet");
-       	$data = curl_exec($ch); 
-        	curl_close($ch);
-			if($xmlFlag){
-	            header("Content-Type: text/xml");
-	      }else{
-	            header("Content-Type: text/json");
-	      }
-        echo $data;
+        echo $response;
+		
+		curl_close($ch);
 ?>
