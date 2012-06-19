bccb13128fed8887757d78ca755a6968624cfb0c
John Gomez <johncar@gmail.com>
2012-06-19 16:02:36 +0000

    Simplest and well docummented code

--- src/main/javascript/Biojs.HelloWorld.js
+++ src/main/javascript/Biojs.HelloWorld.js
@@ -9,14 +9,14 @@
  * @version 1.0.0
  * 
  * @requires <a href='http://code.jquery.com/jquery-1.6.4.js'>jQuery Core 1.6.4</a>
- * @dependency <script language="JavaScript" type="text/javascript" src="../biojs/dependencies/jquery/jquery-1.4.2.min.js"></script>
+ * @dependency <script language="JavaScript" type="text/javascript" src="../biojs/dependencies/jquery/jquery-1.6.4.min.js"></script>
  * 
  * @param {Object} options An object with the options for HelloWorld component.
  *    
  * @option {string} target 
  *    Identifier of the DIV tag where the component should be displayed.
  *    
- * @option {string} [fontFamily=ï¿½ï¿½Andale monoï¿½, courier, monospaceï¿½] 
+ * @option {string} [fontFamily='"Andale mono", courier, monospace'] 
  *    Font list to be applied to the component content.
  *  
  * @option {string} [fontColor="white"] 
@@ -42,11 +42,18 @@ Biojs.HelloWorld = Biojs.extend (
 /** @lends Biojs.HelloWorld# */
 {
   constructor: function (options) {
+	  // In JavaScript ÒthisÓ always refers to the ÒownerÓ of the function we're executing (http://www.quirksmode.org/js/this.html)
+	  // Let's preserve the reference to 'this' through the variable self. In this way, we can invoke/execute 
+	  // our component instead of the object where 'this' is being invoked/executed.
 	  var self = this;
 	  
+	  // For practical use, create an object with the main DIV container 
+	  // to be used in all of the code of our component
+	  this._container = jQuery("#"+self.opt.target);
+	  
 	  // Apply options values
-	  jQuery("#"+self.opt.target).css({
-		  'font-family': self.opt.fontFamily,
+	  this._container.css({
+		  'font-family': self.opt.fontFamily, // this is one example of the use of self instead of this
 		  'background-color': self.opt.backgroundColor,
 		  'color': self.opt.fontColor,
 		  'font-size': '36px',
@@ -60,7 +67,7 @@ Biojs.HelloWorld = Biojs.extend (
 	  // Disable text selection and
 	  // Change the selection mouse pointer  
 	  // from text to hand.
-	  jQuery("#"+self.opt.target).css({
+	  this._container.css({
 		  '-moz-user-select':'none',
 		  '-webkit-user-select':'none',
 		  'user-select':'none'
@@ -68,14 +75,16 @@ Biojs.HelloWorld = Biojs.extend (
 
 	  // Set the content
 	  text = 'Hello World!';
-	  contentHTML = '';
-	  for( i=0; i< text.length; i++ ) {
-		  contentHTML += '<span>' + text[i] + '</span>';
-	  }	  
-	  jQuery( contentHTML ).appendTo( "#"+self.opt.target );  
 
-	  // Internal method to initialize mouse events
-	  self._addSelectionTrigger();
+	  for( i=0; i< text.length; i++ ) {
+		  this._container.append('<span>' + text[i] + '</span>');
+	  }	   
+
+	  // Internal method to initialize the event of select 'Hello'  
+	  this._addSelectionTrigger();
+	  
+	  // Internal method to set the onClick event 
+	  this._addSimpleClickTrigger();
   },
 
   /**
@@ -96,6 +105,23 @@ Biojs.HelloWorld = Biojs.extend (
    * @name Biojs.HelloWorld-eventTypes
    */
   eventTypes : [
+	/**
+	 * @name Biojs.HelloWorld#onClick
+	 * @event
+	 * @param {function} actionPerformed A function which receives an {@link Biojs.Event} object as argument.
+	 * @eventData {Object} source The component which did triggered the event.
+	 * @eventData {string} type The name of the event.
+	 * @eventData {int} selected Selected character.
+	 * @example 
+	 * instance.onClick(
+	 *    function( objEvent ) {
+	 *       alert("The character " + objEvent.selected + " was clicked.");
+	 *    }
+	 * ); 
+	 * 
+	 * */
+	 "onClick",
+	 
 	/**
 	 * @name Biojs.HelloWorld#onHelloSelected
 	 * @event
@@ -133,51 +159,85 @@ Biojs.HelloWorld = Biojs.extend (
 	  var self = this;
 	  var isMouseDown = false;
 	  
-	  /**
-	   * @private
-	   * @function
-	   */
-	  var toggleClass = function (span) {
-		  span.toggleClass( 'selected' );
-			if ( span.hasClass('selected') ) {
-				span.css('color', self.opt.selectionFontColor)
-					.css("background-color", self.opt.selectionBackgroundColor);
-			} else {
-				span.css('color', self.opt.fontColor)
-					.css("background-color", self.opt.backgroundColor);
-			}
-	  };
+	  // Create the CSS class called selected to change both background and color 
 
-	  jQuery( '#' + self.opt.target ).find('span')
+	  jQuery('<style> .selected { '+
+		  	'background-color:' + self.opt.selectionBackgroundColor + ';' +
+		  	'color:' + self.opt.selectionFontColor +'; }</style>'
+	  		).appendTo('head');
+	  
+	  //
+	  // Add the click event to each character in the content
+	  // But remember, we must to figure out when 'Hello' is selected only
+	  this._container.find('span')
 	  	.mousedown(function() {
+	  		
+	  		// Turn on the flag 
 			isMouseDown = true;
-			jQuery('#' + self.opt.target + ' span' ).each( function() {
-				jQuery(this).removeClass('selected')
-					.css('color', self.opt.fontColor)
-					.css("background-color", self.opt.backgroundColor);
-			});
-			toggleClass( jQuery(this) );
-		})
-		.mouseover(function() {
+			
+			// A new selection is starting
+			// Reset all by removing the CSS "selected" class if already applied
+			self._container.children('span').removeClass('selected')
+			
+			// Apply the class for this span/character
+			// NOTE: "this" refers to the internal object 'span'
+			// NOT to the component's instance
+			jQuery(this).addClass('selected');
+				
+	  	}).mouseover(function() {
+	  		// Check if the mouse is being dragged  
 			if (isMouseDown) {
-				toggleClass( jQuery(this) );
+				jQuery(this).addClass('selected');
 			}	
 		})
 		.mouseup(function() {
+			
+			/// Turn off the flag 
 			isMouseDown = false;
+			
 			var textSelected = '';
 			
-			jQuery('#' + self.opt.target + ' span.selected' )
-			.each(function(){
-				textSelected += jQuery(this).text();
-			});
+			// Get the entire selected word
+			self._container.children('span.selected')
+				.each(function(){
+					textSelected += jQuery(this).text();
+				});
 			
+			// Since requirements, only "Hello" word should be selected 
+			// to raise the event
 			if (textSelected == 'Hello') {
 				self.raiseEvent('onHelloSelected', {
 					textSelected : textSelected
 				})
 			}
 		});
+  },
+  
+  _addSimpleClickTrigger: function () {
+	  
+	  var self = this;
+	  
+	  // Add the click event to each character in the content
+	  this._container.find('span')
+	  	.click( function(e) {
+	  		// A letter was clicked!
+	  		// Let's discover which one was it
+	  		// TIP: e.target contains the clicked DOM node
+	  		var selected = jQuery(e.target).text();
+	  		
+	  		// Create an event object 
+	  		var evtObject = { "selected": selected };
+	  		
+	  		// We're ready to raise the event onClick of our component
+	  		self.raiseEvent('onClick', evtObject);
+	  	});
   }
   
 });
+
+
+
+
+
+
+
