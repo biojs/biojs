7d01b7cf4bdc424d975a07cb421d5c7c5e31b39d
John Gomez <johncar@gmail.com>
2012-10-16 21:48:21 +0000

    added category tag

--- src/main/javascript/Biojs.DasProteinFeatureViewer.js
+++ src/main/javascript/Biojs.DasProteinFeatureViewer.js
@@ -6,6 +6,7 @@
  *
  * @author <a href="mailto:ljgarcia@ebi.ac.uk">Leyla Jael Garcia Castro</a>
  * @version 1.0.0
+ * @category 3
  *
  * @requires <a href='http://code.jquery.com/jquery-1.6.2.min.js/'>jQuery Core 1.4.2</a>
  * @dependency <script language="JavaScript" type="text/javascript" src="../biojs/dependencies/jquery/jquery-1.4.2.min.js"></script>
