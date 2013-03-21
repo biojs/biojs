9be4b69ccae2772de0b225da7de3b401e005c54a
swanand <swanand@gmail.com>
2013-03-21 23:21:41 +0000

    PDBchainTopology widget 	fitToBox() had to be added because raphael.setViewBox() has issues in IE and safari

--- src/main/javascript/Biojs.PDBchainTopology.js
+++ src/main/javascript/Biojs.PDBchainTopology.js
@@ -91,7 +91,7 @@ Biojs.PDBchainTopology = Biojs.extend (
 		self.respaths = []; // essential to store hoverable residue elems so that they can be brought 'up' after rendeing domain
 		self.resi2paths = {};
 		self.thinssXscale = 1.0; self.allYscale = 1.0; self.allXscale = 1.5;
-		self.curvyloops = true; //true;
+		self.curvyloops = false; //true;
 		self.loopdip = 'useArea';
 
 		//self.topodata = {"coils": [{"start": 1, "stop": 3, "path": [203.077, 69.231, 203.077, 62.821000000000026, 203.077, 56.41100000000006, 228.718, 56.41100000000006, 228.718, 114.10300000000001, 203.077, 114.10300000000001, 203.077, 120.51300000000003]}, {"start": -1, "stop": -1, "path": [203.077, 235.89800000000002, 203.077, 242.30800000000005, 203.077, 255.12900000000002, 203.077, 261.53900000000004]}, {"start": 22, "stop": 23, "path": [203.077, 376.923, 203.077, 383.33400000000006, 151.795, 383.33400000000006, 151.795, 370.51300000000003, 151.795, 364.10300000000007]}, {"start": 37, "stop": 38, "path": [151.795, 197.43600000000004, 151.795, 191.026, 151.795, 178.20500000000004, 151.795, 171.79500000000002]}, {"start": 46, "stop": 46, "path": [151.795, 82.05200000000002, 151.795, 75.64100000000008, 151.795, 62.821000000000026, 100.513, 62.821000000000026, 100.513, 69.231]}, {"start": 56, "stop": 57, "path": [100.513, 184.61600000000004, 100.513, 191.026, 49.231, 191.026, 49.231, 184.61600000000004]}, {"start": 67, "stop": 69, "path": [49.231, 69.231, 49.231, 62.821000000000026, 49.231, 50.0, 510.769, 50.0, 510.769, 255.12900000000002, 510.769, 261.53900000000004]}, {"start": 75, "stop": 77, "path": [510.769, 325.641, 510.769, 332.052, 459.487, 332.052, 459.487, 325.641]}, {"start": 89, "stop": 89, "path": [459.487, 184.61600000000004, 459.487, 178.20500000000004, 408.205, 178.20500000000004, 408.205, 184.61600000000004]}, {"start": 100, "stop": 104, "path": [408.205, 312.821, 408.205, 319.23100000000005, 356.923, 319.23100000000005, 356.923, 293.59000000000003, 356.923, 287.18]}, {"start": 114, "stop": 116, "path": [356.923, 171.79500000000002, 356.923, 165.38500000000005, 305.641, 165.38500000000005, 305.641, 171.79500000000002]}, {"start": 126, "stop": 126, "path": [305.641, 287.18, 305.641, 293.59000000000003, 254.359, 293.59000000000003, 254.359, 280.77000000000004, 254.359, 274.35900000000004]}, {"start": -1, "stop": -1, "path": [254.359, 146.15400000000005, 254.359, 139.74400000000003, 254.359, 126.923, 254.359, 120.51300000000003]}], "terms": [{"stop": 0, "resnum": "1", "path": [190.256, 82.05150000000003, 203.0765, 69.231, 215.897, 82.05150000000003, 203.0765, 94.87200000000007], "type": "N", "start": 0}, {"stop": 136, "resnum": "137", "path": [267.179, 94.87200000000007, 267.179, 120.51300000000003, 241.538, 120.51300000000003, 241.538, 94.87200000000007], "type": "C", "start": 136}], "extents": [30.0, 50.0, 530.0, 383.33400000000006], "domains": {"CATH": {"2.40.128.20": [[[0, 136]]]}, "PFAM": {"PF00061": [[[3, 136]]]}, "SCOP": {"b.60.1.2": [[[0, 136]]]}}, "helices": [{"start": 24, "minoraxis": 8.974, "stop": 36, "majoraxis": 17.949, "path": [133.846, 351.28200000000004, 169.744, 210.257]}, {"start": 13, "minoraxis": 8.974, "stop": 21, "majoraxis": 17.949, "path": [185.128, 274.35900000000004, 221.026, 364.10300000000007]}], "reslist": [["   1", " ", "PRO"], ["   2", " ", "ASN"], ["   3", " ", "PHE"], ["   4", " ", "SER"], ["   5", " ", "GLY"], ["   6", " ", "ASN"], ["   7", " ", "TRP"], ["   8", " ", "LYS"], ["   9", " ", "ILE"], ["  10", " ", "ILE"], ["  11", " ", "ARG"], ["  12", " ", "SER"], ["  13", " ", "GLU"], ["  14", " ", "ASN"], ["  15", " ", "PHE"], ["  16", " ", "GLU"], ["  17", " ", "GLU"], ["  18", " ", "LEU"], ["  19", " ", "LEU"], ["  20", " ", "LYS"], ["  21", " ", "VAL"], ["  22", " ", "LEU"], ["  23", " ", "GLY"], ["  24", " ", "VAL"], ["  25", " ", "ASN"], ["  26", " ", "VAL"], ["  27", " ", "MET"], ["  28", " ", "LEU"], ["  29", " ", "ARG"], ["  30", " ", "LYS"], ["  31", " ", "ILE"], ["  32", " ", "ALA"], ["  33", " ", "VAL"], ["  34", " ", "ALA"], ["  35", " ", "ALA"], ["  36", " ", "ALA"], ["  37", " ", "SER"], ["  38", " ", "LYS"], ["  39", " ", "PRO"], ["  40", " ", "ALA"], ["  41", " ", "VAL"], ["  42", " ", "GLU"], ["  43", " ", "ILE"], ["  44", " ", "LYS"], ["  45", " ", "GLN"], ["  46", " ", "GLU"], ["  47", " ", "GLY"], ["  48", " ", "ASP"], ["  49", " ", "THR"], ["  50", " ", "PHE"], ["  51", " ", "TYR"], ["  52", " ", "ILE"], ["  53", " ", "LYS"], ["  54", " ", "THR"], ["  55", " ", "SER"], ["  56", " ", "THR"], ["  57", " ", "THR"], ["  58", " ", "VAL"], ["  59", " ", "ARG"], ["  60", " ", "THR"], ["  61", " ", "THR"], ["  62", " ", "GLU"], ["  63", " ", "ILE"], ["  64", " ", "ASN"], ["  65", " ", "PHE"], ["  66", " ", "LYS"], ["  67", " ", "VAL"], ["  68", " ", "GLY"], ["  69", " ", "GLU"], ["  70", " ", "GLU"], ["  71", " ", "PHE"], ["  72", " ", "GLU"], ["  73", " ", "GLU"], ["  74", " ", "GLN"], ["  75", " ", "THR"], ["  76", " ", "VAL"], ["  77", " ", "ASP"], ["  78", " ", "GLY"], ["  79", " ", "ARG"], ["  80", " ", "PRO"], ["  81", " ", "CYS"], ["  82", " ", "LYS"], ["  83", " ", "SER"], ["  84", " ", "LEU"], ["  85", " ", "VAL"], ["  86", " ", "LYS"], ["  87", " ", "TRP"], ["  88", " ", "GLU"], ["  89", " ", "SER"], ["  90", " ", "GLU"], ["  91", " ", "ASN"], ["  92", " ", "LYS"], ["  93", " ", "MET"], ["  94", " ", "VAL"], ["  95", " ", "CYS"], ["  96", " ", "GLU"], ["  97", " ", "GLN"], ["  98", " ", "LYS"], ["  99", " ", "LEU"], [" 100", " ", "LEU"], [" 101", " ", "LYS"], [" 102", " ", "GLY"], [" 103", " ", "GLU"], [" 104", " ", "GLY"], [" 105", " ", "PRO"], [" 106", " ", "LYS"], [" 107", " ", "THR"], [" 108", " ", "SER"], [" 109", " ", "TRP"], [" 110", " ", "THR"], [" 111", " ", "ARG"], [" 112", " ", "GLU"], [" 113", " ", "LEU"], [" 114", " ", "THR"], [" 115", " ", "ASN"], [" 116", " ", "ASP"], [" 117", " ", "GLY"], [" 118", " ", "GLU"], [" 119", " ", "LEU"], [" 120", " ", "ILE"], [" 121", " ", "LEU"], [" 122", " ", "THR"], [" 123", " ", "MET"], [" 124", " ", "THR"], [" 125", " ", "ALA"], [" 126", " ", "ASP"], [" 127", " ", "ASP"], [" 128", " ", "VAL"], [" 129", " ", "VAL"], [" 130", " ", "CYS"], [" 131", " ", "THR"], [" 132", " ", "ARG"], [" 133", " ", "VAL"], [" 134", " ", "TYR"], [" 135", " ", "VAL"], [" 136", " ", "ARG"], [" 137", " ", "GLU"]], "strands": [{"start": 58, "stop": 66, "path": [36.41, 184.61600000000004, 36.41, 88.46199999999999, 30.0, 88.46199999999999, 49.231, 69.231, 68.462, 88.46199999999999, 62.051, 88.46199999999999, 62.051, 184.61600000000004]}, {"start": 47, "stop": 55, "path": [113.333, 69.231, 113.333, 165.38500000000005, 119.744, 165.38500000000005, 100.513, 184.61600000000004, 81.282, 165.38500000000005, 87.692, 165.38500000000005, 87.692, 69.231]}, {"start": 39, "stop": 45, "path": [138.974, 171.79500000000002, 138.974, 101.28200000000004, 132.564, 101.28200000000004, 151.795, 82.05200000000002, 171.026, 101.28200000000004, 164.615, 101.28200000000004, 164.615, 171.79500000000002]}, {"start": 4, "stop": 12, "path": [215.897, 120.51300000000003, 215.897, 216.66700000000003, 222.308, 216.66700000000003, 203.077, 235.89800000000002, 183.846, 216.66700000000003, 190.256, 216.66700000000003, 190.256, 120.51300000000003]}, {"start": 127, "stop": 136, "path": [241.538, 274.35900000000004, 241.538, 165.38500000000005, 235.128, 165.38500000000005, 254.359, 146.15400000000005, 273.59, 165.38500000000005, 267.179, 165.38500000000005, 267.179, 274.35900000000004]}, {"start": 117, "stop": 125, "path": [318.462, 171.79500000000002, 318.462, 267.949, 324.872, 267.949, 305.641, 287.18, 286.41, 267.949, 292.821, 267.949, 292.821, 171.79500000000002]}, {"start": 105, "stop": 113, "path": [344.103, 287.18, 344.103, 191.026, 337.692, 191.026, 356.923, 171.79500000000002, 376.154, 191.026, 369.744, 191.026, 369.744, 287.18]}, {"start": 90, "stop": 99, "path": [421.026, 184.61600000000004, 421.026, 293.59000000000003, 427.436, 293.59000000000003, 408.205, 312.821, 388.974, 293.59000000000003, 395.385, 293.59000000000003, 395.385, 184.61600000000004]}, {"start": 78, "stop": 88, "path": [446.667, 325.641, 446.667, 203.846, 440.256, 203.846, 459.487, 184.61600000000004, 478.718, 203.846, 472.308, 203.846, 472.308, 325.641]}, {"start": 70, "stop": 74, "path": [523.59, 261.53900000000004, 523.59, 306.41100000000006, 530.0, 306.41100000000006, 510.769, 325.641, 491.538, 306.41100000000006, 497.949, 306.41100000000006, 497.949, 261.53900000000004]}]};
@@ -427,6 +427,7 @@ Biojs.PDBchainTopology = Biojs.extend (
 		self.scaleYall();
 		self.scaleXall();
 		self.editLoopsHorizontals();
+		self.fitToBox();
 		if(self.checkDataSanity(topodata) == false) { alert("Data error!! Cannot continue."); return; }
 		var ssattrib = {'stroke-width':1,'stroke':'#aaa'};
 		var unmappedattrib = {'stroke-dasharray':'- .','stroke':'#aaa'}; // ["", "-", ".", "-.", "-..", ". ", "- ", "--", "- .", "--.", "--.."]
@@ -493,12 +494,54 @@ Biojs.PDBchainTopology = Biojs.extend (
 			                "L", (ass.path[0]+ass.path[2]+ass.path[4]+ass.path[6])/4, ass.path[3] ];
 			if(ass.start != -1 && ass.stop != -1) self.makeResidueSubpaths(respath, ass.start, ass.stop);
 		}
-		// final resizing
+		// final resizing // not required anymore due to fitToBox. setViewBox was not working in safari, IE properly anyway
+		// var extents = self.findExtents();
+		// var minx = extents[0]; var miny = extents[1];
+		// var maxx = extents[2]; var maxy = extents[3];
+		// var margin = 50;
+		// self.config.rapha.setViewBox(minx-margin, miny-margin, maxx-minx+2*margin, maxy-miny+2*margin, true); // TODO issue in IE, safari ! try patching raphael.js see https://github.com/DmitryBaranovskiy/raphael/issues/468 doesnt work even with it...
+		// self.config.rapha.setViewBox(minx-margin, miny-margin, maxx-minx+2*margin, maxy-miny+2*margin); // TODO issue in IE, safari ! try patching raphael.js see https://github.com/DmitryBaranovskiy/raphael/issues/468 doesnt work even with it...
+	},
+
+	fitToBox: function() {
+		var self = this;
+		// find transform to go from p,q to a,b
+		var margin = 10;
+		var ax=margin, ay=margin, bx=self.config.size-margin, by=self.config.size-margin;
 		var extents = self.findExtents();
-		var minx = extents[0]; var miny = extents[1];
-		var maxx = extents[2]; var maxy = extents[3];
-		var margin = 50;
-		self.config.rapha.setViewBox(minx-margin, miny-margin, maxx-minx+2*margin, maxy-miny+2*margin, true); // TODO issue in IE, safari ! try patching raphael.js see https://github.com/DmitryBaranovskiy/raphael/issues/468 doesnt work even with it...
+		var px=extents[0] ,py=extents[1], qx=extents[2], qy=extents[3];
+		// find scale
+		var scale = (bx-ax)/(qx-px);
+		if(scale > (by-ay)/(qy-py))
+			scale = (by-ay)/(qy-py);
+		// transform
+		var sstypes = {coils:"red", strands:"green", helices:"blue", terms:'purple'};
+		for(var st in sstypes) {
+			eval("var sselems = self.topodata."+st+";");
+			for(var ci=0; ci < sselems.length; ci++) {
+				var ass = sselems[ci];
+				for(var pi=0; pi < ass.path.length; pi+=2) {
+					ass.path[pi+0] = (ass.path[pi+0]-px) * scale + ax;
+					ass.path[pi+1] = (ass.path[pi+1]-py) * scale + ay;
+				}
+				ass.majoraxis *= scale; ass.minoraxis *= scale;
+			}
+		}
+		// center
+		extents = self.findExtents();
+		dx = (extents[0]+extents[2])/2 - (ax+bx)/2 ;
+		dy = (extents[1]+extents[3])/2 - (ay+by)/2 ;
+		// transform
+		for(var st in sstypes) {
+			eval("var sselems = self.topodata."+st+";");
+			for(var ci=0; ci < sselems.length; ci++) {
+				var ass = sselems[ci];
+				for(var pi=0; pi < ass.path.length; pi+=2) {
+					ass.path[pi+0] -= dx;
+					ass.path[pi+1] -= dy;
+				}
+			}
+		}
 	},
 
 	makeResidueSubpaths: function(fullpath, startresi, stopresi, reverse) {
