9937020645162d47520208f5f06774084cb35d9a
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2014-06-10 11:57:53 +0100

    removed bootstrap and changed to simpler styling to remove conflicts with biojs defaults

--- src/main/javascript/Biojs.OPSCompoundInfo.js
+++ src/main/javascript/Biojs.OPSCompoundInfo.js
@@ -25,9 +25,6 @@
   * @requires <a href='../biojs/css/biojs.openphacts.css'>Open PHACTS CSS</a>
   * @dependency <link href="../biojs/css/biojs.openphacts.css" rel="stylesheet" type="text/css" />
   *
-  * @requires <a href='http://getbootstrap.com/2.3.2/'>Bootstrap css</a>
-  * @dependency <link href="../biojs/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css" />
-  *
   * @param {Object} options An object with the options for the component.
   *
   * @option {string} appID
@@ -63,7 +60,7 @@
              var callback = function(success, status, response) {
                  var compoundResult = searcher.parseCompoundResponse(response);
                  jQuery('#' + self.opt.target);
-                 var template = '<div class="row"><div class="container"><div class="container-fluid large-top-spacing"><div class="row-fluid"><div class="span3"><div style="padding:0 0 0 20px;background:white;">{{cs_image_src csURI}}</div>';
+                 var template = '<div><div><div class="large-top-spacing"><div"><div class="left"><div style="padding:0 0 0 20px;background:white;">{{cs_image_src csURI}}</div>';
                  template += '<div id="boxes" class="small-top-spacing">{{#if logp}}<div class="summary-box"><div class="inner-property-content"><p><small>AlogP</small></p><p class="lead">{{logp}}</p></div></div>{{/if}}';
                  template += '{{#if hba}}<div class="summary-box"><div class="inner-property-content"><p><small># H-Bond Acceptors</small></p><p class="lead">{{hba}}</p></div></div>{{/if}}';
                  template += '{{#if hbd}}<div class="summary-box"><div class="inner-property-content"><p><small># H-Bond Donors</small></p><p class="lead">{{hbd}}</p></div></div>{{/if}}';
@@ -71,15 +68,15 @@
                  template += '{{#if mwFreebase}}<div class="summary-box"><div class="inner-property-content"><p><small>Mol Weight Freebase</small></p><p class="lead">{{mwFreebase}}</p></div></div>{{/if}}';
                  template += '{{#if psa}}<div class="summary-box"><div class="inner-property-content"><p><small>Polar Surface Area</small></p><p class="lead">{{psa}}</p></div></div>{{/if}}';
                  template += '{{#if rtb}}<div class="summary-box"><div class="inner-property-content"><p><small># Rotatable Bonds</small></p><p class="lead">{{rtb}}</p></div></div>{{/if}}</div></div>';
-                 template += '<div class="span9"><div><div style="padding:20px 0 0 0"><h1>{{prefLabel}}</h1></div><div class="lead pull-right"><p class="pull-left">{{formatMolecularFormula molform}}</p></div></div>';
+                 template += '<div><div><div><h1>{{prefLabel}}</h1></div><p class="pull-left">{{formatMolecularFormula molform}}</p></div>';
                  template += '<div class="summary-descriptive-text large-top-spacing">{{#if description}}<p>{{description}}</p>{{/if}}</div>';
                  template += '<div class="summary-descriptive-text small-top-spacing">{{#if biotransformation}}<p>{{biotransformationItem}}</p>{{/if}}</div>';
-                 template += '<div class="dl-horizontal" style="font-size:13px;"><dl>{{#if csURI}}<dt class="medium-padding-top">ChemSpider ID</dt><dd class="medium-padding-top">{{linkableChemspiderID csURI}}</dd>{{/if}}';
-                 template += '{{#if smiles}}<dt class="medium-padding-top">SMILES</dt><dd class="medium-padding-top">{{smiles}}</dd>{{/if}}';
-                 template += '{{#if inchi}}<dt class="medium-padding-top">Standard  InChI</dt><dd class="medium-padding-top">{{inchi}}</dd>{{/if}}';
-                 template += '{{#if inchiKey}}<dt class="medium-padding-top">Standard InChIKey</dt><dd class="medium-padding-top">{{inchiKey}}</dd>{{/if}}';
-                 template += '{{#if proteinBinding}}<dt class="medium-padding-top">Protein Binding</dt><dd class="medium-padding-top">{{proteinBinding}}</dd>{{/if}}';
-                 template += '{{#if toxicity}}<dt class="medium-padding-top">Toxicity</dt><dd class="medium-padding-top">{{toxicity}}</dd>{{/if}}</dl></div></div></div></div></div></div>';
+                 template += '<div class="dl-horizontal" style="font-size:13px;"><dl>{{#if csURI}}<dt class="medium-padding-top big-font bold-font">ChemSpider ID</dt><dd class="medium-padding-top">{{linkableChemspiderID csURI}}</dd>{{/if}}';
+                 template += '{{#if smiles}}<dt class="medium-padding-top big-font bold-font">SMILES</dt><dd class="medium-padding-top">{{smiles}}</dd>{{/if}}';
+                 template += '{{#if inchi}}<dt class="medium-padding-top big-font bold-font">Standard  InChI</dt><dd class="medium-padding-top">{{inchi}}</dd>{{/if}}';
+                 template += '{{#if inchiKey}}<dt class="medium-padding-top big-font bold-font">Standard InChIKey</dt><dd class="medium-padding-top">{{inchiKey}}</dd>{{/if}}';
+                 template += '{{#if proteinBinding}}<dt class="medium-padding-top big-font bold-font">Protein Binding</dt><dd class="medium-padding-top">{{proteinBinding}}</dd>{{/if}}';
+                 template += '{{#if toxicity}}<dt class="medium-padding-top big-font bold-font">Toxicity</dt><dd class="medium-padding-top">{{toxicity}}</dd>{{/if}}</dl></div></div></div></div></div></div>';
                  var hbsTemplate = Handlebars.compile(template);
                  var html = hbsTemplate(compoundResult);
                  jQuery('#' + self.opt.target).replaceWith(html);
