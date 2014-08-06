1d1d925438ec2db063967f46a8753dea90b57180
daviddao <contact.daviddao@gmail.com>
2014-08-06 10:28:38 +0000

    changed package.json

--- biojs-vis-tree/index.js
+++ biojs-vis-tree/index.js
@@ -1,2 +1,3 @@
+//Biojs-vis-tree based on code of TNT by Miguel Pignatelli and edited for BioJS 2.0
 module.exports = require('./src/index');
 module.exports.parse_newick = require('biojs-io-newick').newick;
