d6f6086a8699502f30ef3476395190df862f7804
daviddao <contact.daviddao@gmail.com>
2014-08-06 10:30:44 +0000

    chanegs

--- biojs-vis-tree/index.js
+++ biojs-vis-tree/index.js
@@ -1,3 +1,2 @@
-//Biojs-vis-tree based on code of TNT by Miguel Pignatelli and edited for BioJS 2.0
 module.exports = require('./src/index');
 module.exports.parse_newick = require('biojs-io-newick').newick;
