b9524c4ebe6f3cdef91173403c48cadb5d190f12
4ndr01d3 <gustavoadolfo.salazar@gmail.com>
2014-08-14 10:22:29 +0200

    removing biojs-util-area_selector from the repository updating biojs-vis-chromosome to point to the ne location fo the area_selector.

--- biojs-util-area_selector/browser.js
+++
@@ -1,7 +0,0 @@
-if (typeof biojs === 'undefined') {
-  module.exports = biojs = {}
-}
-if (typeof biojs.vis === 'undefined') {
-  module.exports = biojs.util = {}
-}
-biojs.util.area_selector = require('./')
