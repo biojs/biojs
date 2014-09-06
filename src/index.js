06fe6d10f171dbdad14473d7c597d27b841ebe6e
Sebastian Wilzbach <sebi@wilzbach.me>
2014-09-06 18:10:19 +0200

    cleanup of the template

--- src/index.js
+++ src/index.js
@@ -6,38 +6,6 @@
 * Can you build a parser to analyze this SNP file?
 */
 
+var template = {};
 
-var snipspector = {};
-
-snipspector.parse = function() {
-    
-    var data = ["rs5747620	20	15412698	TT",
-    "rs9605903	20	15434720	CC",
-    "rs2236639	20	15452483	GC",
-    "rs5747999	21	15455353	AA",
-    "rs11089263	21	15467656	A-",
-    "rs2096537	21	15474749	AC",
-    "rs9604959	22	15479107	CG",
-    "rs9604967	22	15492342	CC"];
-
-    var chromosomes = [];
-
-    // analyze SNPs
-    // homo(zygous): AA
-    // hetero(zygous): AC
-    // del(etion): A-, -A or --
-    for (var i = 0; i < data.length; i++) {
-        // Please fill in your code here! 
-    }
-
-    console.log(chromosomes); 
-
-    return chromosomes;
-}
-
-snipspector.parse(); //Should print [{name: "20", homo: 2, hetero: 1, del: 0,
-                     // {name: "21", homo: 1, hetero: 1, del: 1}, 
-                     // {name: "22", homo 1, hetero: 1, del: 0 }]
-
-
-module.exports = snipspector; // Export the object for other components
+module.exports = template; // Export the object for other components
